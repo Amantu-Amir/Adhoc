6fa6ecbf2f481442197ba7b5f959c071806236a1
Amir Hosen <42311923+amir-hosen7@users.noreply.github.com>
2023-02-04 11:57:12 +0600

    Create 567. Permutation in String.cpp

---
+++ Two-Pointer/LeetCode/567. Permutation in String.cpp
@@ -0,0 +1,34 @@
+//slide size s1.size()
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> v(30, 0), cnt(30, 0);
+        for (auto i : s1) {
+            v[i - 'a' + 1]++;
+        }
+        int siz = s1.size(), n = s2.size(), l = -1, flag = 0;
+        for (int i = 0; i < n; i++) {
+            if (i >= (siz - 1)) {
+                cnt[s2[i] - 'a' + 1]++;
+                if (l != -1) {
+                    cnt[s2[l] - 'a' + 1]--;
+                }
+                bool ok = 1;
+                for (int j = 1; j <= 26; j++) {
+                    if (cnt[j] != v[j]) {
+                        ok = 0; break;
+                    }
+                }
+                if (ok) {
+                    flag = 1; break;
+                }
+                l++;
+            }
+            else {
+                cnt[s2[i] - 'a' + 1]++;
+            }
+        }
+        return flag;
+    }
+};
